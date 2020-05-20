213fba012f1aefa0109d9a9efc66c6370e019d9b
Seth Greenbaum <sethmgreenbaum@gmail.com>
2020-05-19 21:02:08 -0600

    fixed proxy for server call, tested api for posts

--- client/src/pages/FavoritesPage.js
+++ client/src/pages/FavoritesPage.js
@@ -1,2 +1,2 @@
-import React, { Component } from "react";
-import Favorites from "../Components/LoginModal/index";
+// import React, { Component } from "react";
+// import Favorites from "../Components/LoginModal/index";
