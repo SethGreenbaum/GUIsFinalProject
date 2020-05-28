7028edd5b7c952b56fbedc36e61cba62f54367ab
Seth Greenbaum <sethmgreenbaum@gmail.com>
2020-05-28 10:24:20 -0600

    added funcitonality to like button

--- client/src/Components/LikeBtn/index.js
+++ client/src/Components/LikeBtn/index.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState }from "react";
 import "./style.css";
 import logo from "./like-btn.png";
 import M from "materialize-css";
+import ls from "local-storage";
+import API from "../../utils/API/linklikeAPI";
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
 
 
 
-function LikeBtn(props) {
-
+function LikeBtn({title}) {
+  const [linklikeState, setLinklikeState] = useState({
+    username: ls.get('username'),
+    link: title
+  });
+  const handleLike = () =>  {
+    console.log('props'+ title)
+    var body = linklikeState
+    console.log(body)
+    API.createLinkLike(body)
+    .then(
+      console.log("post liked")
+    )
+    .catch(error => {
+      console.log('like error: ')
+      console.log(error);
+    })
+  }
   return (
-    <span className="like-btn" {...props} role="button" tabIndex="0">
-      <img id="like-btn" src={logo} alt="like button" />
+    <span className="like-btn"  role="button" tabIndex="0">
+      <img id="like-btn" src={logo} alt="like button" onClick={()=>handleLike()} />
     </span>
   );
 }
